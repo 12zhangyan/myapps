50580bee68b5b3d3f487d8935895d247b1e564ef
java张岩 <2251873002@qq.com>
2023-07-05 19:12:51 +0800

    20230705

---
+++ src/pages/Admin/UserManage/index.tsx
@@ -0,0 +1,206 @@
+import { PlusOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { Button } from 'antd';
+import { useRef } from 'react';
+import {searchUsers} from "@/services/ant-design-pro/api";
+
+const columns: ProColumns<API.CurrentUser>[] = [
+  {
+    dataIndex: 'id',
+    valueType: 'indexBorder',
+    width: 48,
+  },
+  {
+    title: '用户名',
+    dataIndex: 'username',
+    //是否允许复制
+    copyable: true,
+    //是否允许缩略
+    //ellipsis: true,
+  },
+  {
+    title: '用户账户',
+    dataIndex: 'userAccount',
+    //是否允许复制
+    copyable: true,
+  },
+  {
+    title: '头像',
+    dataIndex: 'avatarUrl',
+    copyable: true,
+  },
+  {
+    title: '性别',
+    dataIndex: 'gender',
+  },
+  {
+    title: '电话',
+    dataIndex: 'phone',
+    copyable: true,
+  },
+  {
+    title: '邮件',
+    dataIndex: 'email',
+    copyable: true,
+  },
+  {
+    title: '状态',
+    dataIndex: 'userStatus',
+  },
+  {
+    title: '角色',
+    dataIndex: 'role',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    //声明这一列的类型
+    valueType: 'dateTime',
+  },
+
+  // {
+  //   disable: true,
+  //   title: '状态',
+  //   dataIndex: 'state',
+  //   filters: true,
+  //   onFilter: true,
+  //   ellipsis: true,
+  //   valueType: 'select',
+  //   valueEnum: {
+  //     all: { text: '超长'.repeat(50) },
+  //     open: {
+  //       text: '未解决',
+  //       status: 'Error',
+  //     },
+  //     closed: {
+  //       text: '已解决',
+  //       status: 'Success',
+  //       disabled: true,
+  //     },
+  //     processing: {
+  //       text: '解决中',
+  //       status: 'Processing',
+  //     },
+  //   },
+  // },
+  // {
+  //   disable: true,
+  //   title: '标签',
+  //   dataIndex: 'labels',
+  //   search: false,
+  //   renderFormItem: (_, { defaultRender }) => {
+  //     return defaultRender(_);
+  //   },
+  //   render: (_, record) => (
+  //     <Space>
+  //       {record.labels.map(({ name, color }) => (
+  //         <Tag color={color} key={name}>
+  //           {name}
+  //         </Tag>
+  //       ))}
+  //     </Space>
+  //   ),
+  // },
+  {
+    title: '操作',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        编辑
+      </a>,
+      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+        查看
+      </a>,
+      <TableDropdown
+        key="actionGroup"
+        onSelect={() => action?.reload()}
+        menus={[
+          { key: 'copy', name: '复制' },
+          { key: 'delete', name: '删除' },
+        ]}
+      />,
+    ],
+  },
+];
+
+export default () => {
+  const actionRef = useRef<ActionType>();
+  return (
+    <ProTable<API.CurrentUser>
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      request={async (params = {}, sort, filter) => {
+        console.log(sort, filter);
+        const userList = await searchUsers();
+        //await waitTime(2000);
+        return {
+          data: userList
+        }
+      }}
+      editable={{
+        type: 'multiple',
+      }}
+      columnsState={{
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
+      form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
+      dateFormatter="string"
+      headerTitle="高级表格"
+    />
+      // pagination={{
+      //   pageSize: 5,
+      //   onChange: (page) => console.log(page),
+      // }}
+      // dateFormatter="string"
+      // headerTitle="高级表格"
+      // toolBarRender={() => [
+      //   <Button
+      //     key="button"
+      //     icon={<PlusOutlined />}
+      //     onClick={() => {
+      //       actionRef.current?.reload();
+      //     }}
+      //     type="primary"
+      //   >
+      //     新建
+      //   </Button>,
+
+      // ]}
+  );
+};
